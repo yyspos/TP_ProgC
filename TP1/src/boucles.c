4fa307d18b2c70008dce67793694a8366843f25d
ademosoff <adalebrazi@gmail.com>
2025-04-16 15:00:45 +0200

    Update boucles.c

---
+++ TP1/src/boucles.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int compteur;
+
+    printf("Entrez une valeur pour compteur (inférieure à 10) : ");
+    scanf("%d", &compteur);
+
+    if (compteur >= 10) {
+        printf("Erreur : compteur doit être inférieur à 10.\n");
+        return 1;
+    }
+
+    printf("\nTriangle avec des boucles for :\n");
+    for (int i = 1; i <= compteur; i++) {
+        for (int j = 1; j <= i; j++) {
+            // Première et dernière ligne : que des étoiles
+            if (i == 1 || i == compteur) {
+                printf("* ");
+            }
+            // Première et dernière position sur la ligne : étoiles
+            else if (j == 1 || j == i) {
+                printf("* ");
+            }
+            // Le reste : des dièses
+            else {
+                printf("# ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
