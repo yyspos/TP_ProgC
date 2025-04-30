b96dabbd03ac4317e25b4e2893e9b54edefc5ff5
Yann <137297146+yyspos@users.noreply.github.com>
2025-04-30 15:13:43 +0200

    Update opérateurs.c

---
+++ TP1/src/opérateurs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a = 16;
+    int b = 3;
+
+    // Opérations arithmétiques
+    int addition = a + b;
+    int soustraction = a - b;
+    int multiplication = a * b;
+    int division = a / b; // division entière
+    int modulo = a % b;
+
+    // Opérateurs de comparaison
+    int est_egal = (a == b);       // 1 si vrai, 0 sinon
+    int est_superieur = (a > b);   // 1 si vrai, 0 sinon
+
+    // Affichage des résulta
