9fc676132b7d16ecad1f669ae5fbe5598bf0d3fa
Yann <137297146+yyspos@users.noreply.github.com>
2025-04-30 14:44:43 +0200

    Update bonjour.c

--- TP1/src/bonjour.c
+++ TP1/src/bonjour.c
@@ -1,20 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    // On choisit les deux nombres : a est la base, b est l'exposant
-    int a = 2; // par exemple 2
-    int b = 3; // par exemple 3
-
-    // Variable pour stocker le résultat (on commence à 1)
-    int resultat = 1;
-
-    // On répète la multiplication a * a * a ... b fois
-    for (int i = 0; i < b; i++) {
-        resultat = resultat * a;
-    }
-
-    // On affiche le résultat final
-    printf("%d à la puissance %d = %d\n", a, b, resultat);
-
-    return 0; // Fin du programme
+    int a = 2, b = 3, resultat = 1;
+    for (int i = 0; i < b; i++)
+        resultat *= a;
+    printf("%d^%d = %d\n", a, b, resultat);
+    return 0;
 }
