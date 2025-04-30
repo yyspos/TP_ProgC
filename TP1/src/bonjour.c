a87b05ea8941fd0d95dca3b09f87af4afd57c13f
Yann <137297146+yyspos@users.noreply.github.com>
2025-04-30 14:47:03 +0200

    Update bonjour.c

--- TP1/src/bonjour.c
+++ TP1/src/bonjour.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    int a = 2, b = 3, resultat = 1;
-    for (int i = 0; i < b; i++)
-        resultat *= a;
-    printf("%d^%d = %d\n", a, b, resultat);
+    printf("Bonjour le Monde!\n");
     return 0;
 }
